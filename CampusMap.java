5af02a8774dedbc3af10622fa71a2edd33b98249
Cbrodish03 <148904877+Cbrodish03@users.noreply.github.com>
2024-11-25 23:06:07 +0000

    Push unimplemented CampusMap and Pathfinder methods

---
+++ CampusMap.java
@@ -0,0 +1,14 @@
+/**
+ * Intended to handle most of the map information, implementation is TBD
+ * 
+ * @author cbrodish03
+ * @version 11/25/2024
+ */
+public class CampusMap {
+
+    // - Fields
+
+    // - Constructor(s)
+
+    // - Methods
+}
---
+++ Pathfinder.java
@@ -0,0 +1,14 @@
+/**
+ * Container Class for the Project, holding most contents for the A* search agent
+ * 
+ * @author cbrodish03
+ * @version 11/25/2024
+ */
+public class Pathfinder {
+
+    // - Fields
+
+    // - Constructor(s)
+
+    // - Methods
+}
