ff487ec3b9205f27a673f7cff22bc7872e9fdc20
Cbrodish03 <cbrodish03@vt.edu>
2024-12-01 21:42:05 -0500

    Updated CampusGUI and CampusMap on main

--- CampusGUI.java
+++ CampusGUI.java
@@ -1,14 +1,100 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Simple GUI to display the Campus
  * 
  * @author cbrodish03
  * @version 11/26/2024
  */
-public class CampusGUI {
+public class CampusGUI extends JFrame {
 
     // - Fields
 
+    private CampusMap map;
+
     // - Constructor(s)
 
+    /**
+     *
+     */
+    public CampusGUI() {
+        map = new CampusMap();
+        initializeUI();
+    }
+
     // - Methods
+
+    /**
+     *
+     */
+    private void initializeUI() {
+        setTitle("Campus Map GUI");
+        setSize(1000, 1200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MapPanel panel = new MapPanel(map);
+        add(panel);
+        setVisible(true);
+    }
+
+
+    /**
+     * Runs the program
+     *
+     * @param args arguments to run with
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(CampusGUI::new); // Ensure GUI updates happen on the Event Dispatch Thread
+    }
+
+    /**
+     * Custom JPanel to display the graph
+     */
+    private static class MapPanel extends JPanel {
+
+        // - Fields
+        private final CampusMap map;
+
+        // - Constructor(s)
+
+        /**
+         * Modified constructor, taking the map as a parameter
+         *
+         * @param map the CampusMap object
+         */
+        public MapPanel(CampusMap map) {
+            this.map = map;
+        }
+
+        // - Methods
+
+        /**
+         *
+         * @param g the <code>Graphics</code> object to protect
+         */
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            // Draw edges
+            g.setColor(Color.GRAY);
+            for (Edge edge : map.getEdges()) {
+                int x1 = (int) edge.getFirstNode().getX();
+                int y1 = (int) edge.getFirstNode().getY();
+                int x2 = (int) edge.getSecondNode().getX();
+                int y2 = (int) edge.getSecondNode().getY();
+                g.drawLine(x1, y1, x2, y2);
+            }
+
+            // Draw nodes
+            g.setColor(Color.BLUE);
+            for (Node node : map.getNodes()) {
+                int x = (int) node.getX();
+                int y = (int) node.getY();
+                g.fillOval(x - 5, y - 5, 10, 10); // Draw nodes as small circles
+                g.drawString(node.getName(), x + 10, y); // Label the node
+            }
+        }
+    }
 }
--- CampusMap.java
+++ CampusMap.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 /**
- * Intended to handle most of the map information, implementation is TBD
+ * Intended to handle most of the map information, houses the backend map data
  * 
  * @author cbrodish03
  * @version 11/25/2024
@@ -7,8 +9,45 @@
 public class CampusMap {
 
     // - Fields
+    private final ArrayList<Node> nodes;
+    private final ArrayList<Edge> edges;
 
     // - Constructor(s)
+    /**
+     * Default CampusMap constructor, defining node and edge lists and initializing the data
+     */
+    public CampusMap() {
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
+        initializeMapData();
+    }
 
     // - Methods
+    /**
+     * Initializes the map data by populating the node and edge lists
+     */
+    public void initializeMapData() {
+        // Create nodes
+        Node goodwin = new Node("Goodwin Hall", 300, 200);
+        nodes.add(goodwin);
+        Node dds = new Node("D&DS Bulding", 175, 300);
+        nodes.add(dds);
+
+        // Create edges
+        edges.add(new Edge(goodwin, dds, 5.0));
+    }
+
+    /**
+     * Returns the list of nodes
+     *
+     * @return nodes
+     */
+    public ArrayList<Node> getNodes() { return nodes; }
+
+    /**
+     * Returns the list of edges
+     *
+     * @return edges
+     */
+    public ArrayList<Edge> getEdges() { return edges; }
 }
